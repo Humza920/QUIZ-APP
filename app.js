d849c2e5c07693b9402d6f994ce3ebfbfeaef9bd
Humza920 <humza.rehman.779@gmail.com>
2024-06-09 02:20:22 -0700

    Done

--- app.js
+++ app.js
@@ -1,50 +1,38 @@
-const getData = localStorage.getItem("send")
-const formLogin = document.querySelector("form")
+// GETTING DATA
 
+const getData = localStorage.getItem("send");
 console.log(getData);
-
-let data;
-
-data = JSON.parse(getData)
+const data = JSON.parse(getData);
 console.log(data);
 
-// let rom = data.map( item =>{
-// console.log(item);
-// })
+// HTML ELEMENTS THROUGH JS
 
+const formLogin = document.querySelector("form");
+let email = document.querySelector("#email");
+let pass = document.querySelector("#password");
 
+// LOG / NOR-LOG
 
-formLogin.addEventListener('submit' , (event) =>{
-    event.preventDefault()
-    
-if (getData === null || getData === null) {
-        console.log("Create an account before LogIn");
-}
+formLogin.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-let email = document.querySelector("#email")
-let pass = document.querySelector("#password")
+  if (getData === null || getData === "") {
+    console.log("Create an account before LogIn");
+  }
+  if ( getData?.includes(email.value) && getData?.includes(pass.value) ) {
+    alert("SUCCESFULLY LOGIN");
+    formLogin.reset();
+    window.location = "quiz.html";
+  } else {
+    console.log("incorrect email or pass");
 
+    email.value = "";
+    pass.value = "";
+    alert("incorrect email or pass");
+  }
 
+  email.value = "";
+  pass.value = "";
 
-    if (getData.includes(email.value) && getData.includes(pass.value)) {
-
-        alert("SUCCESFULLY LOGIN");
-        formLogin.reset()
-        window.location = "quiz.html"
-        
-    }  
-        else  {
-            // console.log("incorrect email or pass");
-            
-            email.value = ""
-            pass.value = ""
-            alert("incorrect email or pass");
-        }  
-
-
-
-
-
-        // email.value = ""
-        // pass.value = ""
-})
+});
+// end{code}
